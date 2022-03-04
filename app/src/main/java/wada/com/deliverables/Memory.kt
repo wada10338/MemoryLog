5a55d06d34cd0b733d718ba75ff6a6f6dac46713
wada1 <wada1@MAINDESKTOP>
2022-03-04 22:05:56 +0900

    緯度経度逆だったから直した

--- app/src/main/java/wada/com/deliverables/Memory.kt
+++ app/src/main/java/wada/com/deliverables/Memory.kt
@@ -6,5 +6,5 @@ import androidx.room.PrimaryKey
 @Entity
 data class Memory(
     @PrimaryKey(autoGenerate = true)
-    var id:Int,val title:String,val Contents:String,val longitude:Double,val  latitude:Double ,val Date:String) {
+    var id:Int,val title:String,val Contents:String,val  latitude:Double ,val longitude:Double,val Date:String) {
 }
--- app/src/main/java/wada/com/deliverables/MemoryDao.kt
+++ app/src/main/java/wada/com/deliverables/MemoryDao.kt
@@ -2,14 +2,20 @@ package wada.com.deliverables
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 @Dao
 interface MemoryDao {
+    //データの挿入
     @Insert
     fun insert(memory:Memory)
 
+
     @Query("select * from memory")
     fun getAll():LiveData<List<Memory>>
+
+    @Delete
+    fun delete(memory:Memory)
 }
 //テスト
